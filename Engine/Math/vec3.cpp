f9b9e005a01641e6a27f646ff6ccb7a74cc91b85
Richard Fox <rjfox321@gmail.com>
2015-10-26 21:24:21 +0000

    Basic vector math implementation

--- Engine/Math/vec3.cpp
+++ Engine/Math/vec3.cpp
@@ -2,11 +2,141 @@
 
 using namespace Math;
 
+vec3 vec3::Zero(0, 0, 0);
+
+#pragma region Construction/Destruction
 vec3::vec3()
 {
 }
 
+vec3::vec3(float nx, float ny, float nz) : x(nx), y(ny), z(nz)
+{
+}
+
+Math::vec3::vec3(vec3 &rhs) :x(rhs.x), y(rhs.y), z(rhs.z)
+{
+}
 
 vec3::~vec3()
 {
 }
+#pragma endregion
+
+vec3& Math::vec3::operator=(const vec3 &rhs)
+{
+	x = rhs.x;
+	y = rhs.y;
+	z = rhs.z;
+
+	return *this;
+}
+bool Math::vec3::operator==(const vec3 & rhs)
+{
+	return x == rhs.x && y == rhs.y && z == rhs.z;
+}
+
+
+
+float vec3::Length() const
+{
+	return sqrt(x*x + y*y + z*z);
+}
+
+#pragma region Unary operations
+//Unary minus returns negative of the vector
+vec3 Math::vec3::operator-() const
+{
+	return vec3(-x, -y, -z);
+}
+
+#pragma endregion
+
+#pragma region scalar operations
+vec3 Math::vec3::operator+=(const float s) const
+{
+	return vec3(x+s, y+s, z+s);
+}
+
+vec3 Math::vec3::operator-=(const float s) const
+{
+	return vec3(x-s, y-s, z-s);
+}
+
+vec3 Math::vec3::operator*=(const float s) const
+{
+	return vec3(x*s, y*s, z*s);
+}
+
+vec3 Math::vec3::operator/=(const float s) const
+{
+	float recip = 1.0f / s; // one div by s and multiplying by recip*3 is faster than div by s
+	return vec3(x*recip, y*recip, z*recip);
+}
+
+vec3 &Math::vec3::operator+=(const float s)
+{
+	x += s;
+	y += s;
+	z += s;
+	return *this;
+}
+
+vec3 &Math::vec3::operator-=(const float s)
+{
+	x -= s;
+	y -= s;
+	z -= s;
+	return *this;
+}
+
+vec3 & Math::vec3::operator/=(const float s)
+{
+	float recip = 1.0f / s;
+	x /= recip;
+	y /= recip;
+	z /= recip;
+	return *this;
+}
+
+vec3 & Math::vec3::operator*=(const float s)
+{
+	x *= s;
+	y *= s;
+	z *= s;
+	return *this;
+}
+#pragma endregion
+
+#pragma region vector operations
+vec3 Math::vec3::operator+(const vec3 rhs) const
+{
+	return vec3(x + rhs.x, y + rhs.y, z + rhs.z);
+}
+
+vec3 Math::vec3::operator-(const vec3 rhs) const
+{
+	return vec3(x - rhs.x, y - rhs.y, z - rhs.z);
+}
+
+void Math::vec3::normalize()
+{
+	float magnitudeSquared = x*x + y*y + z*z;
+	if (magnitudeSquared > 0)
+	{
+		float magRecip = 1.0f / magnitudeSquared;
+		x *= magRecip;
+		y *= magRecip;
+		z *= magRecip;
+	}
+}
+vec3 Math::vec3::cross(const vec3 rhs) const
+{
+	return vec3(y * rhs.z - z * rhs.y,
+				z * rhs.x - x * rhs.z,
+				x * rhs.y - y * rhs.x);
+}
+float Math::vec3::dot(const vec3 rhs) const
+{
+	return x*rhs.x + y*rhs.y + z*rhs.z;
+}
+#pragma endregion
--- Engine/Math/vec3.h
+++ Engine/Math/vec3.h
@@ -1,10 +1,44 @@
 #pragma once
+#include <cmath>
+
 namespace Math
 {
 	class vec3
 	{
 	public:
 		vec3();
+		vec3(float x, float y, float z);
+		vec3(vec3& rhs);
 		~vec3();
+
+		vec3 &operator=(const vec3 &rhs);
+		bool operator==(const vec3 &rhs);
+
+		float Length() const;
+
+		vec3 Math::vec3::operator-() const;
+
+		vec3 operator+=(const float s) const;
+		vec3 operator-=(const float s) const;
+		vec3 operator*=(const float s) const;
+		vec3 operator/=(const float s) const;
+
+		vec3 &operator+=(const float s);
+		vec3 &operator-=(const float s);
+		vec3 &operator*=(const float s);
+		vec3 &operator/=(const float s);
+		
+		vec3 operator+(const vec3 rhs) const;
+		vec3 operator-(const vec3 rhs) const;
+
+		void normalize();
+		vec3 cross(const vec3 rhs) const;
+		float dot(const vec3 rhs) const;
+
+		static vec3 Zero;
+	public:
+		float x;
+		float y;
+		float z;
 	};
 }
